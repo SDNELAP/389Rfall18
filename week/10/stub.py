3d6c10f4d98f0cc164bec51a02701c3028e40c81
suhruthn <suhruthn@users.noreply.github.com>
2018-11-21 23:43:21 -0500

    Finished part 1

--- week/10/stub.py
+++ week/10/stub.py
@@ -1,47 +1,96 @@
 #!/usr/bin/env python2
 # from the git repo
 import md5py
+import socket
+import struct
+import binascii
+
+# TODO: Potential improvements
+# - Prompt user for message/malicious
+# - Improved parsing now that we know the format
+# - Break on correct answer
 
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
+message = 'Hello there'
+malicious = 'Hacking is life' # put your malicious message here
 
-message = ''    # original message here
-legit = ''      # a legit hash of secret + message goes here, obtained from signing a message
+# Connect
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('142.93.118.186', 1234))
 
-# initialize hash object with state of a vulnerable hash
+# Get first Notary Prompt
+res = s.recv(1024)
+
+# Respond to Notary Option Menu
+s.send('1\n')
+s.recv(1024)
+
+# Sign the message and recieve hash data
+
+s.send(message + '\n')
+response = s.recv(1024)
+
+# Parse and print hash
+legit = response.split(' ')[-1]
+print 'Actual hash:',legit,'\n'
+
 fake_md5 = md5py.new('A' * 64)
-fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
-
-malicious = ''  # put your malicious message here
+fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.strip().decode('hex'))
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
 
 # fake_hash is the hash for md5(secret + message + padding + malicious)
 fake_hash = fake_md5.hexdigest()
-print(fake_hash)
-
+print 'Fake hash:',fake_hash,'\n'
 
 #############################
 ### STEP 2: Craft payload ###
 #############################
 
-# TODO: calculate proper padding based on secret + message
-# secret is <redacted> bytes long (48 bits)
-# each block in MD5 is 512 bits long
-# secret + message is followed by bit 1 then bit 0's (i.e. \x80\x00\x00...)
-# after the 0's is a bye with message length in bits, little endian style
-# (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
-# craft padding to align the block as MD5 would do it
-# (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+# Read next set of data
+s.recv(1024)
 
-# payload is the message that corresponds to the hash in `fake_hash`
-# server will calculate md5(secret + payload)
-#                     = md5(secret + message + padding + malicious)
-#                     = fake_hash
-payload = message + padding + malicious
+for i in range(6, 16):
+    # Padding has a total of 64 bytes
+    # 8 bytes are dedicated to size encoding in little endian
+    # \x80 is one original byte
+    # the remainder of the size of the padding is determined by length of the original message and the size of secret
+    # Overall padding size = 64 - 8 - 1 - len(message) - i
+    padding = '\x80' 
+    padding_size = 64 - 8 - 1 - len(message) - i
+    
+    padding += ('\x00') * padding_size # Duplicate 0 padding_size times and append to padding
 
-# send `fake_hash` and `payload` to server (manually or with sockets)
-# REMEMBER: every time you sign new data, you will regenerate a new secret!
+    # Format length of secret + message in little endian
+    length = struct.pack('<Q', (i + len(message)) * 8)
+
+    # Construct payload
+    payload = message + padding + length + malicious
+
+    # Respond to Notary Option Menu
+    s.send('2\n')
+    print s.recv(1024)
+
+    # Pass the fake hash to the Notary 
+    s.send(fake_hash + '\n')
+    print s.recv(1024)
+
+    # Send the payload to the Notary
+    s.send(payload + '\n')
+    response = s.recv(1024)
+
+    # Check for flag
+    response = s.recv(1024)
+    if 'CMSC389R' in response:
+        print 'Flag is found\n'
+        print response
+        print binascii.hexlify(payload)
+        break
+    else:
+       print 'Failed'
+
+       
+    
--- week/10/writeup/README.md
+++ week/10/writeup/README.md
@@ -1,18 +1,23 @@
 Writeup 10 - Crypto II
 =====
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Sthitadheesh Nelapatla
+Section: 0101
 
 I pledge on my honor that I have not given or received anyunauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: Sthitadheesh Nelapatla
 
 ## Assignment 10 Writeup
 
 ### Part 1 (70 Pts)
 
+I began this assignment by using the provided notary port to connect manually and understand how the Digital Notary works. I realized that the first argument required by the program is the function that you want to run. Either signing data or testing a signature's validity. Given that part 1 needs us to calculate the forged hash as it says in the stub code, I finished the first step of part 1 by simply communicating with the server and parsing the hash output and saving that to the provided legit value in the stub code. 
+
+With step 2 of part 1, the process of constructing the padding was initially very confusing for me. I realized that the first byte had to be 1 and the remaining padding had to be zeroes. I initially tried using a for loop to append \x00 based on my calculations but that wasn't working for me (most probably because I don't understand the inclusivity of Python's range function). I found a way to generate a string of repeated substrings using the integer multiplication operator on Stack Overflow and used it. I had initial issues because I placed the inital ```padding = '\x80'``` outside the for-loop which caused all of the padding to be appended across multiple iterations. After I found that bug,I finally found the following flag: CMSC389R-{i_still_put_the_M_between_the_DV} 
 
 ### Part 2 (30 Pts)
+
+Referring to the slides, I noticed that gpg command line tool is useful in generating public key private key pairs. By following the link on the slide, 
 
 
